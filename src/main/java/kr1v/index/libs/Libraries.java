9c1f9aaf85d5d13225e924cfd54300d8c9bec1bd
kr1viah <kravpolet@gmail.com>
2026-03-13 11:38:17 +0100

    Add missing entries in Libraries

--- src/main/java/kr1v/index/libs/Libraries.java
+++ src/main/java/kr1v/index/libs/Libraries.java
@@ -3,6 +3,16 @@ package kr1v.index.libs;
 import kr1v.index.util.ConfigLibrary;
 
 public class Libraries {
-	public static final ConfigLibrary MaLiLib = new MaLiLib();
 	public static final ConfigLibrary ClothConfig = new ClothConfig();
+	public static final ConfigLibrary ConfigToolKit = new ConfigToolKit();
+	public static final ConfigLibrary Configurable = new Configurable();
+	public static final ConfigLibrary ForgeConfigAPIPort = new ForgeConfigAPIPort();
+	public static final ConfigLibrary FzzyConfig = new FzzyConfig();
+	public static final ConfigLibrary Gson = new Gson();
+	public static final ConfigLibrary KaleidoConfig = new KaleidoConfig();
+	public static final ConfigLibrary MaLiLib = new MaLiLib();
+	public static final ConfigLibrary MaLiLibAPI = new MaLiLibAPI();
+	public static final ConfigLibrary MidnightLib = new MidnightLib();
+	public static final ConfigLibrary OwoLib = new OwoLib();
+	public static final ConfigLibrary YetAnotherConfigLib = new YetAnotherConfigLib();
 }
