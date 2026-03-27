cf930eb4b4b0dc58745ca5f8c926a2413337efa0
kr1viah <kravpolet@gmail.com>
2026-03-27 19:34:09 +0100

    Add OffsetUtils538

---
+++ src/main/java/kr1v/index/libs/OffsetUtils538.java
@@ -0,0 +1,41 @@
+package kr1v.index.libs;
+
+import kr1v.index.util.*;
+
+import java.util.List;
+
+public class OffsetUtils538 extends ConfigLibrary {
+	public OffsetUtils538() {
+		id = "offsetutils538";
+		name = "OffsetUtils538";
+		side = Side.BOTH;
+		versions = Versions.ALL_LIST;
+		type = Type.LOADER;
+		dependencies = List.of();
+		extraConfigTypes = List.of();
+		extraFeatures = List.of(Feature.CUSTOM_CONFIG_TYPES);
+		configFormats = List.of(ConfigFormat.JSON);
+		manualInitialization = InitMode.AT_MOD_INIT;
+		configMethod = ConfigMethod.of(ConfigMethod.TypeOfClass.EXTENDING, true, ConfigMethod.Waaa.ANNOTATED_PRIMITIVE);
+		guiMethod = GuiMethod.NONE;
+		notes = "";
+		source = "https://codeberg.org/OffsetMods538/OffsetUtils538";
+
+		exampleConfigClass = """
+public final class ConfigClass implements Config {
+	public boolean exampleBoolean = false;
+                                       
+	@Override
+	public Path getConfigDirPath() {
+		return FabricLoader.getInstance().getConfigDir().resolve("offsetutils538-example");
+	}
+	
+	@Override
+	public String getId() {
+	    return "offsetutils538-example/main";
+	}
+}
+""";
+	}
+}
+
