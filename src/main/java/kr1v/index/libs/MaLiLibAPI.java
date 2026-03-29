1822f0cc80f22310bd2938e143f006f2305d5038
kr1viah <kravpolet@gmail.com>
2026-03-29 20:11:53 +0200

    MaLiLib API is compatible with 26.1

--- src/main/java/kr1v/index/libs/MaLiLibAPI.java
+++ src/main/java/kr1v/index/libs/MaLiLibAPI.java
@@ -9,7 +9,7 @@ public class MaLiLibAPI extends ConfigLibrary {
         id = "malilib-api";
         name = "MaLiLib API";
         side = Side.CLIENT;
-        versions = Versions.ALL_LIST.stream().filter(s -> !s.equals("26.1")).toList();
+        versions = Versions.ALL_LIST;
         type = Type.BOTH;
         dependencies = List.of(Dependency.MALILIB);
         extraConfigTypes = List.of(ConfigType.COLOR, ConfigType.HOTKEY, ConfigType.PAIR, ConfigType.BUTTON);
